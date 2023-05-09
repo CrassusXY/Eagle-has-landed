6b1a01b297d5d4910e085d139bdf4bd1c5cdf3f9
CrassusXY <75444051+CrassusXY@users.noreply.github.com>
2023-05-09 22:25:01 +0200

    added game view

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,16 +9,35 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     ui->stackedWidget->setCurrentWidget(ui->page_3);
+    this->setWindowTitle("Orzeł wylądował");
 
+    scene = new QGraphicsScene(ui->page_4);
+    lander = new QGraphicsPixmapItem(QPixmap(":/new/prefix1/pic/lander.png"));
+    lander->setScale(.1);
+    lander->setTransformOriginPoint(100, 100);
+    scene->addItem(lander);
+    ui->viewer->setScene(scene);
+    ConnectControler();
+
+    ui->EngineStatus->setPixmap(QPixmap(":/new/prefix1/pic/redled.png").scaled(15,15,Qt::KeepAspectRatio));
+    connect(controller, &QSerialPort::readyRead, this, &MainWindow::read_Port);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+    controller->close();
+}
+
+void MainWindow::ConnectControler()
+{
     ui->serialPortPrinter->viewport()->setAutoFillBackground(false);
     ui->serialPortPrinter->setTextColor( QColor( "green" ) );
     ui->serialPortPrinter->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     ui->serialPortPrinter->setFrameStyle(QFrame::NoFrame);
     ui->serialPortPrinter->hide();
-    this->setWindowTitle("Orzeł wylądował");
-
     controller = new QSerialPort();
-    controller->setPortName("COM5");
+    controller->setPortName("COM9");
     controller->setBaudRate(QSerialPort::BaudRate::Baud115200);
     controller->setParity(QSerialPort::Parity::NoParity);
     controller->setDataBits(QSerialPort::DataBits::Data8);
@@ -29,19 +48,13 @@ MainWindow::MainWindow(QWidget *parent)
     if(controller->isOpen()){
         qDebug() << "Connected";
         qDebug() << controller->error();
+        ui->ControlerStatus->setPixmap(QPixmap(":/new/prefix1/pic/greenled.png").scaled(15,15,Qt::KeepAspectRatio));
     }
     else {
         qDebug() << "Not connected";
         qDebug() << controller->error();
+        ui->ControlerStatus->setPixmap(QPixmap(":/new/prefix1/pic/redled.png").scaled(15,15,Qt::KeepAspectRatio));
     }
-
-    connect(controller, &QSerialPort::readyRead, this, &MainWindow::read_Port);
-}
-
-MainWindow::~MainWindow()
-{
-    delete ui;
-    controller->close();
 }
 
 void MainWindow::read_Port()
@@ -56,14 +69,16 @@ void MainWindow::read_Port()
         if (data_ready) {
             qDebug() << "Paczka: "  << controller_data;
             ui->serialPortPrinter->append(controller_data);
-            if (controller_data.at(0) == '1'){
+            if (controller_data.at(2) == '1'){
                 ui->serialPortPrinter->append("Pauza!\n");
                 on_testButton_clicked();
             }
-            if (controller_data.at(2) == '1'){
+            if (controller_data.at(4) == '1'){
                 ui->serialPortPrinter->append("Silnik włączony!\n");
                 on_startButton_clicked();
             }
+            float r = controller_data.mid(6).toFloat();
+            lander->setRotation(r);
             controller_data = "";
             data_ready = false;
         }
@@ -72,7 +87,7 @@ void MainWindow::read_Port()
 
 void MainWindow::on_startButton_clicked()
 {
-    ui->centralwidget->setStyleSheet("border-image: url(:/new/prefix1/backgroundGame.png);");
+    ui->centralwidget->setStyleSheet("border-image: url(:/new/prefix1/pic/backgroundGame.png);");
     ui->stackedWidget->setCurrentWidget(ui->page_4);
 }
 
@@ -85,7 +100,7 @@ void MainWindow::on_exitButton_clicked()
 
 void MainWindow::on_testButton_clicked()
 {
-    ui->centralwidget->setStyleSheet("border-image: url(:/new/prefix1/background.png);");
+    ui->centralwidget->setStyleSheet("border-image: url(:/new/prefix1/pic/background.png);");
     ui->stackedWidget->setCurrentWidget(ui->page_3);
     ui->serialPortPrinter->hide();
 }
@@ -93,7 +108,7 @@ void MainWindow::on_testButton_clicked()
 
 void MainWindow::on_devButton_clicked()
 {
-    ui->centralwidget->setStyleSheet("border-image: url(:/new/prefix1/backgroundGame.png);");
+    ui->centralwidget->setStyleSheet("border-image: url(:/new/prefix1/pic/backgroundGame.png);");
     ui->stackedWidget->setCurrentWidget(ui->page_4);
     ui->serialPortPrinter->show();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,11 +2,16 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "ui_mainwindow.h"
 #include <QMainWindow>
 #include <QtSerialPort/QSerialPort>
 #include <QtSerialPort/QSerialPortInfo>
 #include <QDebug>
 #include <QString>
+#include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsPixmapItem>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -26,11 +31,13 @@ private slots:
     void on_exitButton_clicked();
     void on_testButton_clicked();
     void read_Port();
-
+    void ConnectControler();
     void on_devButton_clicked();
 
 private:
     Ui::MainWindow *ui;
+    QGraphicsScene *scene;
+    QGraphicsPixmapItem *lander;
     QSerialPort *controller;
     QString controller_data;
     bool data_ready = false;
